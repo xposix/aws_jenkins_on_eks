01aa46a66ebd7110cf8e86a50bb84b1dfbf33989
Ruben Sancho <ruben.sancho@gmail.com>
2020-01-30 17:20:55 +0000

    Marking subnets as available for EKS

--- 1-ec2base/network.tf
+++ 1-ec2base/network.tf
@@ -14,6 +14,14 @@ module "vpc" {
   enable_dns_hostnames = true
   enable_dns_support   = true
 
+  private_subnet_tags	= {
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+
   tags = {
     Terraform = "true"
     Environment = "dev"
