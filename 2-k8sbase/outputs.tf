1afd7721b3c8eef675a1dee706e73ca88ced77a3
Ruben Sancho <ruben.sancho@gmail.com>
2020-02-05 10:10:27 +0000

    Merging stash

--- 2-k8sbase/outputs.tf
+++ 2-k8sbase/outputs.tf
@@ -1,11 +1,11 @@
 output "kubeconfig" {
-  value         = module.eks-cluster.kubeconfig
-#   description   = 
-  sensitive     = false
+  value       = module.eks-cluster.kubeconfig
+  description = "Content of the kubeconfig file"
+  sensitive   = false
 }
 
 output "path_to_kubeconfig" {
-  value         = module.eks-cluster.kubeconfig_filename
-#   description   = 
-  sensitive     = false
+  value       = module.eks-cluster.kubeconfig_filename
+  description = "Path to the created kubeconfig"
+  sensitive   = false
 }
